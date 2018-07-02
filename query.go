a73d2767eb6645f7a9c299673b0678720c8158b1
jpincas <jon@pincas.co.uk>
2018-07-02 14:49:49 +0200

    basic querying working

--- query.go
+++ query.go
@@ -7,27 +7,40 @@ import (
 )
 
 type Query struct {
-	db      DB
-	keyRoot []byte
-	value   reflect.Value
-	entity  tormentable
+	db       DB
+	keyRoot  []byte
+	value    reflect.Value
+	entity   interface{}
+	entities interface{}
 }
 
-func (db DB) Query(entity tormentable) Query {
+func (db DB) Query(entities interface{}) Query {
+
 	newQuery := Query{}
-	keyRoot, value := getKeyRoot(entity)
+	keyRoot, value := getKeyRoot(entities)
 	newQuery.keyRoot = keyRoot
 	newQuery.value = value
-	newQuery.entity = entity
 	newQuery.db = db
+	newQuery.entities = entities
+
+	// Get the underlying type of the elements of the 'entities' slice
+	// cast to interface and save on the query.
+	// This will be used for unmarhsalling
+	newQuery.entity = reflect.New(value.Type().Elem()).Interface()
 
 	return newQuery
 }
 
-func (q Query) Run() ([]tormentable, error) {
-
-	results := []tormentable{}
+func (q Query) Run() (int, error) {
+	// Work out what prefix to iterate over
 	prefix := makePrefix(q.keyRoot)
+
+	// Cast the 'entity' we have stored on the query to a 'tormentable'
+	// so that it can be unmarshalled
+	entity := q.entity.(tormentable)
+
+	// Set up a slice to accumulate the results
+	results := []tormentable{}
 
 	err := q.db.KV.View(func(txn *badger.Txn) error {
 		it := txn.NewIterator(badger.DefaultIteratorOptions)
@@ -39,20 +52,40 @@ func (q Query) Run() ([]tormentable, error) {
 				return err
 			}
 
-			_, err = q.entity.UnmarshalMsg(val)
+			_, err = entity.UnmarshalMsg(val)
 			if err != nil {
 				return err
 			}
 
-			results = append(results, q.entity)
+			results = append(results, entity)
 		}
 
 		return nil
 	})
 
 	if err != nil {
-		return []tormentable{}, err
+		return 0, err
 	}
 
-	return results, nil
+	// Now we have a slice of 'tormentables'
+
+	// Set up a slice for 'translating' the 'tormentables' into the target slice type
+	rt := reflect.Indirect(reflect.ValueOf(q.entities))
+
+	// Iterate through the result, using 'reflect.Append' to append to the above slice
+	// the underlying type of the result
+	for _, result := range results {
+		rt = reflect.Append(
+			rt,
+			reflect.Indirect(reflect.ValueOf(result)),
+		)
+	}
+
+	// Now set the accumulated, translated results on the original, passed in
+	// 'entities'
+	reflect.Indirect(reflect.ValueOf(q.entities)).Set(rt)
+
+	return len(results), nil
 }
+
+// newQuery.entity = reflect.New(reflect.TypeOf(entities).Elem().Elem()).Interface()
--- query_test.go
+++ query_test.go
@@ -8,25 +8,30 @@ func Test_BasicQuery(t *testing.T) {
 	db, _ := OpenTest("data/tests")
 	defer db.Close()
 
-	// Save a couple of orders
+	// 1 order
 	order1 := Order{}
-	order2 := Order{}
-	db.Save(&order1, &order2)
+	db.Save(&order1)
 
-	// Suboptimal:
-
-	// Run a query for all orders
-	var order Order
-	results, _ := db.Query(&order).Run()
-
-	// Conver the []tormentable -> []orders
 	var orders []Order
-	for _, result := range results {
-		orders = append(orders, *result.(*Order))
+	n, err := db.Query(&orders).Run()
+
+	if err != nil {
+		t.Error("Testing basic querying - got error")
 	}
 
-	// What I really want to do:
-	// var orders []Order
-	// db.Query(&orders).Run()
+	if len(orders) != 1 || n != 1 {
+		t.Errorf("Testing querying with 1 entity saved. Expecting 1 entity - got %v/%v", len(orders), n)
+	}
+
+	// 2 orders
+	order2 := Order{}
+	orders = []Order{}
+	db.Save(&order2)
+
+	n, _ = db.Query(&orders).Run()
+
+	if len(orders) != 2 || n != 2 {
+		t.Errorf("Testing querying with 2 entity saved. Expecting 2 entities - got %v/%v", len(orders), n)
+	}
 
 }
