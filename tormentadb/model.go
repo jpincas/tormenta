bf6142451a874eab84e0fba02da895b242f89798
jpincas <jon@pincas.co.uk>
2018-08-02 18:29:27 +0200

    added LastUpdated back to index

--- tormentadb/model.go
+++ tormentadb/model.go
@@ -21,7 +21,7 @@ type Tormentable interface {
 type Model struct {
 	ID          gouuidv6.UUID `msg:",extension" tormenta:"noindex"`
 	Created     time.Time     `msg:"-" tormenta:"noindex"`
-	LastUpdated time.Time     `tormenta:"noindex"`
+	LastUpdated time.Time
 }
 
 func newID() gouuidv6.UUID {
